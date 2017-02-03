72ac2b61f4289578c9a1b8a3f7259d84f2284f89
Nuno Goncalves <nunojpg@gmail.com>
2017-02-03 12:13:13 +0100

    Use OCSP instead of CRL to check revocation; refactor code
    Signed-off-by: Nuno Goncalves <nunojpg@gmail.com>

--- read-pteid.sh
+++ read-pteid.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 # Copyright (C) 2016-2017, Nuno Goncalves <nunojpg@gmail.com>
 
-# trace and notepad objects are not validated by SOD
-# we verify the SOD certificate with CRL but not OCSP because OpenSSL command line tool doesn't allow it easily
-# document can not be validated (INTERNAL_AUTH/ICAO MRTD AA) because it requires mutual authentication with a government issued CVC
-
-# TODO:
-# read address
+# trace and notepad objects are not validated by SOd
+# document can not be authenticated (INTERNAL_AUTH/ICAO MRTD AA) because it requires mutual authentication with a government issued CVC
 
 error=false
 
@@ -14,16 +10,16 @@ print_all_fields=false
 print_photo_ascii=false
 read_address=false
 print_sod_values=false
-download_crl=true
+do_online_OCSP=true
 
-filename_opensc_cardsearch="opensc_cardsearch"
-filename_citizen_data="object_citizen_data"
-filename_citizen_address="object_citizen_address"
-filename_sod="object_SOD"
-filename_sod_certificate="document_sign_certificate"
-filename_photo_jp2="photo.jp2"
-filename_photo_bmp="photo.bmp"
-filename_photo_jpg="photo.jpg"
+fn_opensc_cardsearch="opensc_cardsearch"
+fn_obj_data="object_citizen_data"
+fn_obj_address="object_citizen_address"
+fn_SOd="object_SOd"
+fn_SOd_cert="doc_sign_cert"
+fn_photo_jp2="photo.jp2"
+fn_photo_bmp="photo.bmp"
+fn_photo_jpg="photo.jpg"
 
 pin_tries_left() {
 	pins_list="$1"
@@ -56,9 +52,9 @@ Read, cryptographically authenticate data, and print it.
     -h          display this help and exit
     -a		Print all identification fields
     -b		Print address
-    -c          Disable CRL download
+    -c          Disable OCSP (online) verification
     -p		Print photo ascii art
-    -s		Print SOD values
+    -s		Print SOd values
 EOF
 }
 
@@ -72,7 +68,7 @@ while getopts "hpabcs" opt; do
             ;;
 	b)  read_address=true
             ;;
-	c)  download_crl=false
+	c)  do_online_OCSP=false
 	    ;;
         p)  print_photo_ascii=true
             ;;
@@ -87,10 +83,10 @@ done
 
 check_package "pkcs15-tool" "opensc"
 
-pins_list="$(pkcs15-tool --wait --verbose --list-pins 2> >(tee $filename_opensc_cardsearch >&2))"
+pins_list="$(pkcs15-tool --wait --verbose --list-pins 2> >(tee $fn_opensc_cardsearch >&2))"
 
-cardsearch="$(<$filename_opensc_cardsearch)"
-rm $filename_opensc_cardsearch
+cardsearch="$(<$fn_opensc_cardsearch)"
+rm $fn_opensc_cardsearch
 if [[ "$cardsearch" != *Found\ CARTAO\ DE\ CIDADAO! ]]; then exit; fi
 
 driver="${pins_list#*driver }"
@@ -100,25 +96,27 @@ auth="$(pin_tries_left "$pins_list" "Auth PIN")"
 sign="$(pin_tries_left "$pins_list" "Sign PIN")"
 address="$(pin_tries_left "$pins_list" "Address PIN")"
 
-printf 'PIN tries left        : [AUTH: %s] [SIGN: %s] [Address: %s]\n' "$auth" "$sign" "$address"
+printf '\nPIN tries left            : [AUTH: %s] [SIGN: %s] [Address: %s]\n' "$auth" "$sign" "$address"
 
-remove_file "$filename_citizen_data"
-remove_file "$filename_citizen_address"
-remove_file "$filename_sod"
-remove_file "$filename_sod_certificate"
-remove_file "$filename_photo_jp2"
-remove_file "$filename_photo_bmp"
+remove_file "$fn_obj_data"
+remove_file "$fn_obj_address"
+remove_file "$fn_SOd"
+remove_file "$fn_SOd_cert"
+remove_file "$fn_photo_jp2"
+remove_file "$fn_photo_bmp"
 
 mkdir -p cache
-pkcs15-tool --read-data-object 'Citizen Data' --output "cache/$filename_citizen_data" > /dev/null 2>&1
+pkcs15-tool --read-data-object 'Citizen Data' --output "cache/$fn_obj_data" > /dev/null 2>&1  || { echo "UNABLE TO RETRIEVE Citizen Data"; exit 1; }
 if $read_address ; then
-	pkcs15-tool --read-data-object 'Citizen Address Data' --output "cache/$filename_citizen_address"
+	echo #new line
+	pkcs15-tool --read-data-object 'Citizen Address Data' --output "cache/$fn_obj_address"  || { echo "UNABLE TO RETRIEVE Address"; exit 1; }
+	echo #new line
 fi
-pkcs15-tool --read-data-object 'SOD' --output "cache/$filename_sod" > /dev/null 2>&1
-#pkcs15-tool --read-data-object 'TRACE' --output "cache/$filename_trace" > /dev/null
+pkcs15-tool --read-data-object 'SOd' --output "cache/$fn_SOd" > /dev/null 2>&1  || { echo "UNABLE TO RETRIEVE SOd"; exit 1; }
 
 # read ID data
-fields=(IssuingEntity
+fields_data=(
+        IssuingEntity
 	Country
 	DocumentType
 	DocumentNumber
@@ -148,24 +146,21 @@ fields=(IssuingEntity
 fields_offsets=(0 40 120 154 182 214 230 250 310 330 450 570 572 578 598 606 624 744 864 984 1104 1122 1144 1162 1282 1312 1342 1372)
 
 i=0
-for field in "${fields[@]}"
-do
-	output="$(dd if=cache/$filename_citizen_data iflag=skip_bytes status=none count=1 skip=${fields_offsets[i]} bs=$((${fields_offsets[$((i+1))]}-${fields_offsets[i]})))"
+for field in "${fields_data[@]}"; do
+	output="$(dd if=cache/$fn_obj_data iflag=skip_bytes status=none count=1 skip=${fields_offsets[i]} bs=$((${fields_offsets[$((i+1))]}-${fields_offsets[i]})))"
 	eval "$field=\"$output\""
 	((i++))
-	if $print_all_fields ; then
-		printf '%-22s: %s\n' "$field" "${!field}"
-	fi
 done
 
 if $read_address ; then
 
-addrType="$(dd if=cache/$filename_citizen_address iflag=skip_bytes status=none count=1 skip=0 bs=2)"
+addrType="$(dd if=cache/$fn_obj_address iflag=skip_bytes status=none count=1 skip=0 bs=2)"
 
 if [ "$addrType" = "N" ]; then
 	is_address_foreign=false;
 	fields_offsets=(0 2 6 10 110 118 218 230 330 350 450 650 670 770 790 830 870 970 1070 1078 1084 1134 1146)
-	fields=(addrType
+	fields_address=(
+		addrType
 		country
 		district
 		districtDesc
@@ -190,7 +185,8 @@ if [ "$addrType" = "N" ]; then
 else
 	is_address_foreign=true;
 	fields_offsets=(0 2 6 106 406 506 606 706 806 818)
-	fields=(addrType
+	fields_address=(
+		addrType
 		country
 		countryDescF
 		addressF
@@ -203,34 +199,37 @@ else
 fi		
 
 i=0
-for field in "${fields[@]}"
-do
-	output="$(dd if=cache/$filename_citizen_address iflag=skip_bytes status=none count=1 skip=${fields_offsets[i]} bs=$((${fields_offsets[$((i+1))]}-${fields_offsets[i]})))"
+for field in "${fields_address[@]}"; do
+	output="$(dd if=cache/$fn_obj_address iflag=skip_bytes status=none count=1 skip=${fields_offsets[i]} bs=$((${fields_offsets[$((i+1))]}-${fields_offsets[i]})))"
 	eval "$field=\"$output\""
 	((i++))
-	if $read_address ; then
-		printf '%-22s: %s\n' "$field" "${!field}"
-	fi
 done
 
 fi
 
 # Save files
 
-f="cache/${DocumentNumberPAN}_$Name $Surname"
+f="cache/${DocumentNumberPAN}_${Name} ${Surname}"
 mkdir -p "$f"
-mv "cache/$filename_citizen_data" "$f"
+mv "cache/$fn_obj_data" "$f"
 if $read_address ; then
-	mv "cache/$filename_citizen_address" "$f"
+	mv "cache/$fn_obj_address" "$f"
 fi
-mv "cache/$filename_sod" "$f"
-#mv "cache/$filename_trace" "$f"
+mv "cache/$fn_SOd" "$f"
+
+fn_obj_data="${f}/$fn_obj_data"
+fn_obj_address="${f}/$fn_obj_address"
+fn_SOd="${f}/$fn_SOd"
+fn_SOd_cert="${f}/$fn_SOd_cert"
+fn_photo_jp2="${f}/$fn_photo_jp2"
+fn_photo_bmp="${f}/$fn_photo_bmp"
+fn_photo_jpg="${f}/$fn_photo_jpg"
 
 # Read Photo
 # photo starts at offset 1583 and goes until the end of the file
 # since the file is 15500 bytes long, the maximum photo size is 13917.
 # removing trailing zero bytes reduces my photo to 13023
-dd if="${f}/$filename_citizen_data" status=none skip=1 bs=1583 | xxd -p | tr -d '\n' | sed 's/\(00\)*$//' | xxd -p -r > "${f}/$filename_photo_jp2"
+dd if="$fn_obj_data" status=none skip=1 bs=1583 | xxd -p | tr -d '\n' | sed 's/\(00\)*$//' | xxd -p -r > "$fn_photo_jp2"
 
 # HASHES
 #the hash order is not the same as the fields are read, so we list them manually.
@@ -261,12 +260,12 @@ hash_id="$({
 	echo -n $HealthNo
 	} | sha256sum -b | head -c 64)"
 
-hash_document_key="$(dd if="${f}/$filename_citizen_data" iflag=skip_bytes status=none count=1 skip=1372 bs=131 | sha256sum -b | head -c 64)"
+hash_document_key="$(dd if="$fn_obj_data" iflag=skip_bytes status=none count=1 skip=1372 bs=131 | sha256sum -b | head -c 64)"
 
 # photo including heading starts at offset 1503 and goes until the end of the file
 # for the hash we need the heading and to remove trailing zeros
 # I didn't find a easy way for this, beside converting it to hexadecimal form first
-hash_photo="$(dd if="${f}/$filename_citizen_data" status=none skip=1 bs=1503 | xxd -p | tr -d '\n' | sed 's/\(00\)*$//' | xxd -p -r | sha256sum -b | head -c 64)"
+hash_photo="$(dd if="$fn_obj_data" status=none skip=1 bs=1503 | xxd -p | tr -d '\n' | sed 's/\(00\)*$//' | xxd -p -r | sha256sum -b | head -c 64)"
 
 if $is_address_foreign ; then
 hash_address="$({
@@ -304,31 +303,38 @@ hash_address="$({
 	} | sha256sum -b | head -c 64)"
 fi
 
-# SOD
+# SOd
 
-# Ignoring the first 4 bytes allows us to parse the SOD file as a CMS/PKCS7 object
+# Ignoring the first 4 bytes allows us to parse the SOd file as a CMS/PKCS7 object
 # Verify certificate and data signature, extract certificate
-tail -c+5 "${f}/$filename_sod" | \
-openssl cms -cmsout -inform DER -verify -CApath CA/ -certsout "${f}/$filename_sod_certificate" > /dev/null || { echo "SOD INVALID"; exit 1; }
+echo -n "Document certificate chain: "
+tail -c+5 "$fn_SOd" | \
+openssl cms -cmsout -inform DER -verify -CApath CA/ -certsout "$fn_SOd_cert" > /dev/null || { echo "SOd INVALID"; exit 1; }
 
-# Verify Certificate signature (again) and CRL (requires internet connection)
-[ "$download_crl" = "true" ] && \
-openssl verify -verbose -crl_download -crl_check -CApath CA/ "${f}/$filename_sod_certificate" > /dev/null || { echo "UNABLE TO VERIFY CRL"; exit 1; }		
+# Verify (online) OCSP
+if $do_online_OCSP ; then
+	OCSP_URI="$(openssl x509 -in "$fn_SOd_cert" -noout -ocsp_uri)"
+	ISSUER_HASH="$(openssl x509 -in "$fn_SOd_cert" -noout -issuer_hash)"
+	echo -n "Document certificate OCSP : "
+	openssl ocsp -issuer "CA/${ISSUER_HASH}.0" -CApath CA/ -url "$OCSP_URI" -cert "$fn_SOd_cert" > /dev/null || { echo "UNABLE TO VERIFY OCSP"; exit 1; }
+fi
+
+echo #new line
 
 hash_id_sod=\
-"$(dd if="${f}/$filename_sod" iflag=skip_bytes status=none count=1 skip=95 bs=32 | xxd -p -c 32)"
+"$(dd if="$fn_SOd" iflag=skip_bytes status=none count=1 skip=95 bs=32 | xxd -p -c 32)"
 
 hash_address_sod=\
-"$(dd if="${f}/$filename_sod" iflag=skip_bytes status=none count=1 skip=134 bs=32 | xxd -p -c 32)"
+"$(dd if="$fn_SOd" iflag=skip_bytes status=none count=1 skip=134 bs=32 | xxd -p -c 32)"
 
 hash_photo_sod=\
-"$(dd if="${f}/$filename_sod" iflag=skip_bytes status=none count=1 skip=173 bs=32 | xxd -p -c 32)"
+"$(dd if="$fn_SOd" iflag=skip_bytes status=none count=1 skip=173 bs=32 | xxd -p -c 32)"
 
 hash_document_key_sod=\
-"$(dd if="${f}/$filename_sod" iflag=skip_bytes status=none count=1 skip=212 bs=32 | xxd -p -c 32)"
+"$(dd if="$fn_SOd" iflag=skip_bytes status=none count=1 skip=212 bs=32 | xxd -p -c 32)"
 
 if $print_sod_values ; then
-	echo "Computed hash							 SOD"
+	echo "Computed hash							 SOd"
 	echo "$hash_id $hash_id_sod"
 	if $read_address ; then
 		echo "$hash_address $hash_address_sod"
@@ -350,17 +356,27 @@ fi
 
 # PRINT STUFF
 
-if ! $print_all_fields ; then
+if $print_all_fields ; then
+	for field in "${fields_data[@]}"; do
+		printf '%-22s: %s\n' "$field" "${!field}"
+	done
+else
 	echo "$Name $Surname     $DateOfBirth"
 	echo "$DocumentNumber     $ValidityEndDate     $LocalofRequest"
-	[ -n "$AccidentalIndications" ] && echo $AccidentalIndications	# IF Accidental Indications exist, they are usually very significative so always print them!
+	# IF Accidental Indications exist, they are usually very significative so always print them!
+	[ -n "$AccidentalIndications" ] && echo $AccidentalIndications
+fi
+
+if $read_address ; then
+	for field in "${fields_address[@]}"; do
+		printf '%-22s: %s\n' "$field" "${!field}"
+	done
 fi
 
 check_package "opj_decompress" "libopenjp2-tools"
-opj_decompress -i "${f}/$filename_photo_jp2" -o "${f}/$filename_photo_bmp" > /dev/null 2>&1
-convert "${f}/$filename_photo_bmp" "${f}/$filename_photo_jpg"
+opj_decompress -i "$fn_photo_jp2" -o "$fn_photo_bmp" > /dev/null 2>&1
+convert "$fn_photo_bmp" "$fn_photo_jpg"
 if $print_photo_ascii ; then
 	check_package "jp2a" "jp2a"
-	jp2a "${f}/$filename_photo_jpg" --height=50
+	jp2a "$fn_photo_jpg" --height=50
 fi
-#eog "${folder}/$filename_photo_bmp"
